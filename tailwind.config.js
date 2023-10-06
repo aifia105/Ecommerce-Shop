611caffd73b1174b2994cca1026fcba57b9269db
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-10-06 03:00:45 +0100

    project-stracture

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    screens:{
+      sm:'576px',
+      md:'769px',
+      lg:'992',
+      xl:'1200px'
+    },
+    container:{
+      center:true,
+      padding: '1rem'
+
+    },
+    extend: {
+      fontFamily:{
+        poppins : "'Poppins' , sans-serif",
+        roboto : "'Roboto', sans-serif;"
+      }
+    },
+  },
+  plugins: [],
+}
+
